9e687eb0ae118359a7824f3c3147b8dc1304fbbd
morses-code <morsescott88@gmail.com>
2020-04-03 09:05:49 +0100

    very crude quiz struct and handler to pull json data

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,9 +9,32 @@ import (
 
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/question", getQuestionHandler)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
+
+func getQuestionHandler(w http.ResponseWriter, r *http.Request) {
+	data, err := json.Marshal(quiz)
+	if err != nil {
+		log.Fatalf("JSON marshaling failed: %s", err)
+	}
+	fmt.Fprintf(w, "%s\n", data)
+}
+
+type Quiz struct {
+	Number int `json:"number"`
+	Question string `json:"question"`
+	Answer string `json:"answer"`
+}
+
+var quiz = []Quiz{
+	{
+		Number:1,
+		Question:"What is the best day of the week?",
+		Answer:"Friday",
+	},
 }
