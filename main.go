37c56fc748c3f9e169f10a16e78f741b8802d1cd
morses-code <morsescott88@gmail.com>
2020-04-06 15:32:33 +0100

    added mongodb, added a sample mongodb connection and document insert

--- main.go
+++ main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 	"morses-code/mc-quizbot/quiz"
 	"net/http"
 )
 
 func main() {
+	ctx := context.TODO()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	collection := client.Database("quiz").Collection("questions")
+
+	collection.InsertOne(ctx, bson.D{
+		{Key: "number", Value: "1"},
+		{Key: "question", Value: "What is the best day of the week?"},
+		{Key: "answer", Value: "Friday"},
+	})
+
 	http.HandleFunc("/", quiz.Handler)
 	http.HandleFunc("/quiz", quiz.GetQuizHandler)
 	http.HandleFunc("/question", quiz.GetQuestionHandler)
